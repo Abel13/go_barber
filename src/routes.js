1604d20d4b858abf224c19c0ca324587d5f7b5b5
abel <abel.o.d@outlook.com>
2019-08-31 03:01:35 -0300

    Class 10 - Creating models loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello Abel" });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Abel Dutra',
+    email: 'abel.o.d@outlook.com',
+    password_hash: 'JJKASDasdaasd9as0',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
